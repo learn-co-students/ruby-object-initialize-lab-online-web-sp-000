990c9f36b8ba68fcadbf7447d4beac09cc1ddd1f
Benjamin M Lancaster <bmlancaster07@gmail.com>
2018-10-25 19:34:12 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,25 @@
+# PERSON AND LIB/PERSON.RB
+
+class Person
+  
+ def initialize(name)
+    @name = name
+  end
+ 
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+
+  def job=(job)
+    @job = job
+  end 
+
+  def job 
+    @job
+  end 
+  
+end
