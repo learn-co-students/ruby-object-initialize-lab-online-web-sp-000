130e126904d359161d3897ea780e1cd576423000
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-12-09 17:48:15 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,32 @@
+class Dog
+  def initialize(breed)
+    @breed=breed
+  end
+  
+  def breed=(breed)
+    @breed=breed
+  end
+  
+  def breed
+    @breed
+  end
+
+
+  def initialize(name)
+    @name=name
+  end
+  
+  def name=(name)
+    @name=name
+  end
+  
+  def name
+    @name
+  end
+end
+
+fido=Dog.new("Fido", "Pug")
+
+fido.name
+
+
