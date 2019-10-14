338f54439d1e900d22ebc99624e7058c78a4d9b6
Bruce Ruffin <bruffin1221@gmail.com>
2019-10-14 04:10:37 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+
+  def initialize(name, breed="Mutt")
+    @name=name
+    @breed=breed
+  end
+
+  def name
+    @bname=name
+  end
+
+  def name
+    @bname
+  end
+
+  def breed
+    @breed=breed
+  end
+
+  def breed
+    @breed
+  end
+
+end
