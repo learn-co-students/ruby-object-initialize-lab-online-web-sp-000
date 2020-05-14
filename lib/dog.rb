756a951a9daedd330dad77bb6300ab1e3ca0e65b
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-05-14 04:39:09 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,9 @@
 class Dog
+  attr_accessor :name, :breed
+
   def initialize(name, breed = "Mutt")
     @name = name
     @breed = breed
   end
-  def name
-    @name
-  end
-  def breed
-    @breed = breed
-  end
+
 end
-    
